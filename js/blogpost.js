a470db50d14da753133d6cadba9e7f6547b6829b
MHerholdt94 <mathiasher@live.no>
2022-10-03 17:47:15 +0200

    Update 0.2 03.10.22
    Started fully on HTML and CSS for most pages. A decent amount of placeholder elements have been added that will be replaced by elements made with JS at a later stage.
    
    -WIP home, about and contact pages (HTML & CSS)

--- js/blogpost.js
+++ js/blogpost.js
@@ -1,24 +1,24 @@
-const baseUrl = "http://localhost/timesignature/wp-json/wp/v2/posts";
-const testContainer = document.querySelector(".test-container");
+// const baseUrl = "http://localhost/timesignature/wp-json/wp/v2/posts";
+// const testContainer = document.querySelector(".test-container");
 
-async function blogPost(url) {
-  try {
-    const response = await fetch(url);
-    const results = await response.json();
+// async function blogPost(url) {
+//   try {
+//     const response = await fetch(url);
+//     const results = await response.json();
 
-    testContainer.innerHTML = "";
+//     testContainer.innerHTML = "";
 
-    for (let i = 0; i < results.length; i++) {
-      const post = results[0];
+//     for (let i = 0; i < results.length; i++) {
+//       const post = results[0];
 
-      console.log(post);
+//       console.log(post);
 
-      testContainer.innerHTML = `${post.title.rendered}${post.content.rendered}`;
-    }
-  } catch (error) {
-    console.log(error);
-    testContainer.innerHTML = "An error occured";
-  }
-}
+//       testContainer.innerHTML = `${post.title.rendered}${post.content.rendered}`;
+//     }
+//   } catch (error) {
+//     console.log(error);
+//     testContainer.innerHTML = "An error occured";
+//   }
+// }
 
-blogPost(baseUrl);
+// blogPost(baseUrl);
