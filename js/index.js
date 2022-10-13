60d88623735aebb38b43712ce61621248b8bedd0
MHerholdt94 <mathiasher@live.no>
2022-10-13 13:49:07 +0200

    RepoCopy Update 2
    Copied over current repo from private GitHub

---
+++ js/index.js
@@ -0,0 +1,35 @@
+const baseUrl =
+  "http://localhost/timesignature/wp-json/wp/v2/posts?_embed&per_page=12";
+
+const sliderPosts = document.querySelector(".slider-posts");
+
+async function blogList(url) {
+  try {
+    const response = await fetch(url);
+    const posts = await response.json();
+
+    sliderPosts.innerHTML = "";
+
+    for (let i = 0; i < posts.length; i++) {
+      if (i === 3) {
+        break;
+      }
+
+      const post = posts[i];
+
+      // console.log(post);
+
+      sliderPosts.innerHTML += `<a href="blog.html?id=${post.id}" class="slider-post" style="background-image: url(${post._embedded["wp:featuredmedia"]["0"].source_url});">
+                                    <div class="slider-details">
+                                        <h3>${post.title.rendered}</h3>
+                                        <p class="post-category">${post._embedded["wp:term"]["0"]["0"].name}</p>
+                                        <div class="slider-excerpt">${post.excerpt.rendered}</div>
+                                    </div>
+                                </a>`;
+    }
+  } catch (error) {
+    console.log("An error has occured", error);
+  }
+}
+
+blogList(baseUrl);
---
+++ js/post.js
@@ -0,0 +1,48 @@
+const title = document.querySelector("#title");
+const container = document.querySelector(".post-container");
+const postCenter = document.querySelector(".post-center");
+const postHeader = document.querySelector("#postHeader");
+const postSub = document.querySelector("#postSub");
+const breadcrumb = document.querySelector("#postBreadcrumb");
+
+const queryString = document.location.search;
+
+const params = new URLSearchParams(queryString);
+
+const id = params.get("id");
+
+const url =
+  "http://localhost/timesignature/wp-json/wp/v2/posts/" + id + "?_embed";
+
+async function postContent() {
+  try {
+    const response = await fetch(url);
+
+    const post = await response.json();
+
+    title.innerHTML = `TIME/SIGNATURE | ${post.title.rendered}`;
+    breadcrumb.innerHTML = `/ ${post.title.rendered}`;
+    postHeader.innerHTML = `${post.title.rendered}`;
+    postSub.innerHTML = `<span class="header-author">Mathias B. Herholdt</span>
+                        <span class="header-date">${post.date}</span>
+                        <span class="header-category">${post._embedded["wp:term"]["0"]["0"].name}</span>`;
+
+    postCenter.style.background = `-webkit-linear-gradient(left,
+                                    rgba(0,0,0,1) 0%,
+                                    rgba(0,0,0,0.3) 15%,
+                                    rgba(0,0,0,0.3) 85%,
+                                    rgba(0,0,0,1) 100%),
+                                    url(${post._embedded["wp:featuredmedia"]["0"].source_url})`;
+
+    container.innerHTML = `<div class="post-details">
+                                <div class="post-content">
+                                    ${post.content.rendered}
+                                </div>
+                            </div>`;
+  } catch (error) {
+    console.log(error);
+    container.innerHTML = "An error occured when fetching blog post details";
+  }
+}
+
+postContent();
