f17c380c98a325f3a1e3f6e2caa61c05deea6cf9
MHerholdt94 <mathiasher@live.no>
2022-10-20 17:03:34 +0200

    Update v0.6.1 20.10.22
    - Nearly finished with home page "latest posts" carousel

--- js/components/carousel.js
+++ js/components/carousel.js
@@ -1,28 +1,116 @@
 document.addEventListener("click", (e) => {
-  let handle;
+  let button;
 
-  if (e.target.matches(".handle")) {
-    handle = e.target;
+  if (e.target.matches(".carousel-btn")) {
+    button = e.target;
   } else {
-    handle = e.target.closest(".handle");
+    button = e.target.closest(".carousel-btn");
   }
 
-  if (handle != null) onHandleClick(handle);
+  if (button != null) onButtonClick(button);
 });
 
-function onHandleClick(handle) {
-  const carousel = handle
+function onButtonClick(button) {
+  const progressBar = button
+    .closest(".index-latest")
+    .querySelector(".progress-bar");
+
+  const carousel = button
     .closest(".carousel-container")
     .querySelector(".carousel");
+
   const carouselIndex = parseInt(
     getComputedStyle(carousel).getPropertyValue("--carousel-index")
   );
 
-  if (handle.classList.contains("left-handle")) {
-    carousel.style.setProperty("--carousel-index", carouselIndex + 1);
+  const progressBarItemCount = progressBar.children.length;
+
+  if (button.classList.contains("prev")) {
+    if (carouselIndex - 1 < 0) {
+      carousel.style.setProperty("--carousel-index", progressBarItemCount - 1);
+      progressBar.children[carouselIndex].classList.remove("active");
+      progressBar.children[progressBarItemCount - 1].classList.add("active");
+    } else {
+      carousel.style.setProperty("--carousel-index", carouselIndex - 1);
+      progressBar.children[carouselIndex].classList.remove("active");
+      progressBar.children[carouselIndex - 1].classList.add("active");
+    }
   }
 
-  if (handle.classList.contains("right-handle")) {
-    carousel.style.setProperty("--carousel-index", carouselIndex - 1);
+  if (button.classList.contains("next")) {
+    if (carouselIndex + 1 >= progressBarItemCount) {
+      carousel.style.setProperty("--carousel-index", 0);
+      progressBar.children[carouselIndex].classList.remove("active");
+      progressBar.children[0].classList.add("active");
+    } else {
+      carousel.style.setProperty("--carousel-index", carouselIndex + 1);
+      progressBar.children[carouselIndex].classList.remove("active");
+      progressBar.children[carouselIndex + 1].classList.add("active");
+    }
   }
 }
+
+document.querySelectorAll(".progress-bar").forEach(calculateProgressBar);
+
+function calculateProgressBar(progressBar) {
+  const carousel = progressBar
+    .closest(".index-latest")
+    .querySelector(".carousel");
+
+  const itemCount = carousel.children.length;
+
+  const itemsPerScreen = parseInt(
+    getComputedStyle(carousel).getPropertyValue("--items-per-screen")
+  );
+
+  const carouselIndex = parseInt(
+    getComputedStyle(carousel).getPropertyValue("--carousel-index")
+  );
+
+  const progressBarItemCount = Math.ceil(itemCount / itemsPerScreen);
+
+  for (let i = 0; i < progressBarItemCount; i++) {
+    const barItem = document.createElement("div");
+
+    barItem.classList.add("progress-item");
+
+    if (i === carouselIndex) {
+      barItem.classList.add("active");
+    }
+
+    progressBar.append(barItem);
+  }
+}
+
+// Throttle (unsure if needed)
+
+// const throttleProgressBar = throttle(() => {
+//   document.querySelectorAll(".progress-bar").forEach(calculateProgressBar);
+// }, 250);
+
+// window.addEventListener("resize", throttleProgressBar);
+
+// function throttle(cb, delay = 1000) {
+//   let shouldWait = false;
+//   let waitingArgs;
+//   const timeoutFunc = () => {
+//     if (waitingArgs == null) {
+//       shouldWait = false;
+//     } else {
+//       cb(...waitingArgs);
+//       waitingArgs = null;
+//       setTimeout(timeoutFunc, delay);
+//     }
+//   };
+
+//   return (...args) => {
+//     if (shouldWait) {
+//       waitingArgs = args;
+//       return;
+//     }
+
+//     cb(...args);
+//     shouldWait = true;
+//     setTimeout(timeoutFunc, delay);
+//   };
+// }
